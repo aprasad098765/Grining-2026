be6fb06359a440804c0e6996d751a218f24256c3
Arjun Prasad <aprasad098765@gmail.com>
2026-09-10 01:20:29 +0530

    simple rnn model training and prediction

--- Simple_RNN_Project/main.py
+++ Simple_RNN_Project/main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from simple-rnn-project!")
+from tensorflow.keras.models import load_model
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.utils import pad_sequences
+import streamlit as st
 
 
-if __name__ == "__main__":
-    main()
+word_index = imdb.get_word_index()
+reverse_word_index = {value:key for key,value in word_index.items()}
+
+model = load_model('simple_rnn_model.h5')
+
+def decode_review(encoded_review):
+    return ' '.join([reverse_word_index.get(i - 3,'?') for i in encoded_review])
+
+def preprocessed_text(text):
+    words = text.lower().split()
+    encoded_review = [word_index.get(word, 2) + 3 for word in words]
+    padded_review = pad_sequences([encoded_review],maxlen = 500)
+    return padded_review
+
+def final_prediction(review):
+    preprocessedtext = preprocessed_text(review)
+    prediction = model.predict(preprocessedtext)
+    
+    sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
+    return sentiment, prediction[0][0]
